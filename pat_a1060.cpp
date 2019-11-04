857c50605df21ff7031a48837462c6acdcf5e2e2
luer <yangmm@buaa.edu.cn>
2019-11-04 18:29:07 +0800

    update the comment

--- pat_a1060.cpp
+++ pat_a1060.cpp
@@ -23,6 +23,21 @@ Note: Simple chopping is assumed without rounding.
  *
  * */
 
+
+
+/*
+ * 思路：
+ * 输入是数字，但长度很长
+ * 输出是0.xxx的科学计数法表示的数字
+ *
+ * 主要是对输入的字符串进行处理：
+ * 1.如果字符串小于1，即0.多少，0.去掉，之后的如果还有n个0，10的指数就是-n，如0.001是10^-2
+ *
+ * 2.如果大于1，题目中提到可能有前导0，先去掉他们，然后算到首位数字到小数点前一位或者最后一位（没有小数点），指数就是位数
+ *
+ * 3.根据结果比较小鼠后面部分和整数部分
+ * */
+
 string deal(string s, int & e, const int n)
 {
     int i = 0;
@@ -101,137 +116,3 @@ int main()
 
 
 
-
-/*给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
-
-'.' 匹配任意单个字符
-'*' 匹配零个或多个前面的那一个元素
-所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/regular-expression-matching
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
- 用队列？
-
- 从头开始，一个一个匹配
- 1.字符不一样，继续
- 2.字符一样或者'.' 进行匹配
-    2.1
-
- * */
-
-class Solution {
-public:
-    bool isMatch(string s, string p) {
-        bool mathching = false;
-        if(s.empty()) return true;
-        else if(p.empty()) return false;
-        int i = 0;
-
-        for(;i < p.size();++i)
-        {
-            auto c = p[i];
-            if(c == s[0] or c == '.')
-            {
-                mathching = !mathching;
-            }
-            else
-                continue;
-        }
-
-    }
-
-
-
-};
-
-/*
-class Solution
-{
-public:
-
-    int myAtoi(string str)
-    {
-        //if(str == NULL)   return 0;
-        int i = 0;
-        bool isNegative = false;
-        int res = 0;
-        while(str[i] == ' ') {i++;}
-        if(str[i] == '-')
-        {
-            isNegative = true;
-        }else if(str[i] == '+')
-        {
-
-        }
-        else if(isdigit(str[i]))
-        {
-            i--;
-        }
-        else
-        {
-            return 0;
-        }
-        i++;
-        for(;i < str.size();++i)
-        {
-            int r = str[i] - '0';
-            if(!isdigit(str[i]))
-            {
-                break;
-            }
-            else
-            {
-                if(res > INT_MAX/10 or (res == INT_MAX and r > 7))
-                {
-                    res = isNegative ? INT_MIN : INT_MAX;
-                    return res;
-                }
-            }
-            res = res * 10 + r;
-        }
-        return isNegative ? -res: res;
-    }
-};
-
-int main()
-{
-    string str = "-2147483648";
-
-    Solution a;
-
-    cout<<a.myAtoi(str)<<endl;
-}
-*/
-/*
-class Solution {
-public:
-    string convert(string s, int numRows) {
-        if(numRows == 1)return s;
-        vector<string> row(min(numRows,int(s.size())));
-        int curRow = 0;
-        bool goingDown = false;
-
-        for(char c : s)
-        {
-            row[curRow]+=c;
-            if(curRow == 0 or curRow == numRows - 1)goingDown = !goingDown;
-
-            curRow += goingDown ? 1 : -1;
-
-        }
-        string res;
-        for(string r : row)res += r;
-        return res;
-
-    }
-};
-
-
-int main() {
-    Solution b;
-    cout<<b.convert("leetcode",3)<<endl;
-    return 0;
-}
-*/
